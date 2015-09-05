0d0659976d3eabd91c524a02859824984c8247dc
Dmitriy Zakharkin <mail@zdima.net>
2015-09-04 21:26:37 -0400

    Initial Commit

---
+++ LMZGaugeView/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LMZGaugeView
+//
+//  Created by Dmitriy Zakharkin on 9/4/15.
+//  Copyright (c) 2015 ZDima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
