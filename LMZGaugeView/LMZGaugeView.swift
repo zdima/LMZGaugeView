0ff437d21af4385aef5013ed11c3e012796f4356
zdima <mail@zdima.net>
2017-01-09 20:21:29 -0500

    fix crash when all data set to 0

--- LMZGaugeView/LMZGaugeView.swift
+++ LMZGaugeView/LMZGaugeView.swift
@@ -426,7 +426,7 @@ open class LMZGaugeView: NSView {
 			let smoothedPath = NSBezierPath()
 
 			let a1 = 180-(startAngle).toDegree
-			let a2 = (self.maxValue.isNaN || self.maxValue.isZero) ? 180 : 180-((endAngle - startAngle) * doubleValue / self.maxValue).toDegree
+			var a2 = (self.maxValue.isNaN || self.maxValue.isZero) ? 180 : 180-((endAngle - startAngle) * doubleValue / self.maxValue).toDegree
 			if a2.isNaN {
 				a2 = a1
 			}
